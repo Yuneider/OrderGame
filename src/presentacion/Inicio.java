8d702e643b176b7248d0c538348c14fbe30e24e5
Alex Diaz <alexsander.diaz64@gmail.com>
2022-02-21 19:25:19 -0500

    interface

--- src/presentacion/Inicio.java
+++ src/presentacion/Inicio.java
@@ -4,8 +4,9 @@
  */
 package presentacion;
 
-import java.awt.Color;
+import java.awt.*;
 import javax.swing.*;
+import javax.swing.border.*;
 
 /**
  *
@@ -14,6 +15,7 @@ import javax.swing.*;
 public class Inicio extends JFrame{
     
     private JPanel fondo;
+    private JLabel menu;
     private JButton jugar, salir, score;
     private ImageIcon ifondo;
     
@@ -25,31 +27,45 @@ public class Inicio extends JFrame{
         setResizable(false);
         setLocationRelativeTo(null);
         
+        ifondo = new ImageIcon("images/menu.png");
+        
         fondo = new JPanel();
         fondo.setLayout(null);
-        fondo.setBackground(Color.red);
         this.add(fondo);
+        
+        menu = new JLabel("dfsraga");
+        menu.setLayout(null);
+        menu.setSize(550,520);
+        menu.setIcon(new ImageIcon(ifondo.getImage().getScaledInstance(550, 520, Image.SCALE_AREA_AVERAGING)));
+        fondo.add(menu);
                 
         jugar = new JButton("Jugar");
         jugar.setBounds(232, 230, 85, 42);
         jugar.setFocusable(false);
-        fondo.add(jugar);
+        jugar.setCursor(new Cursor(Cursor.HAND_CURSOR));
+        jugar.setBorder(new EmptyBorder(10,10,10,10)); 
+        menu.add(jugar);
         
         score = new JButton("Score");
         score.setBounds(232, 300, 85, 42);
         score.setFocusable(false);
-        fondo.add(score);
+        score.setCursor(new Cursor(Cursor.HAND_CURSOR));
+        menu.add(score);
         
         salir = new JButton("Salir");
         salir.setBounds(232, 370, 85, 42);
         salir.setFocusable(false);
-        fondo.add(salir);
+        salir.setCursor(new Cursor(Cursor.HAND_CURSOR));
+        
+        menu.add(salir);
         
         this.setVisible(true);
         
     }
     
-   
+    public static void main (String [] args ) {
+	Inicio show = new Inicio();
+    }
 }
 
 
